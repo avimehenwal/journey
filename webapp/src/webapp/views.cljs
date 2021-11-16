ed9e6c38947be43328707220c04b023ca36551ff
avimehenwal <avi.mehanwal@gmail.com>
2021-11-16 21:13:13 +0100

    feat: add reframe components

--- webapp/src/webapp/views.cljs
+++ webapp/src/webapp/views.cljs
@@ -4,9 +4,25 @@
    [webapp.subs :as subs]
    ))
 
+(defn sample-component []
+  [:div.card [:footer.card-footer [:a.card-footer-item {:href "#"} "save"]]])
+
+(defn extract-attr [el]
+  ;; (cljs.pprint/pprint [1 2 3])
+  (.log js/console el)
+  )
+
+(defn button []
+  (let [b-id "b1"] 
+  [:div 
+   [:button 
+    {:data-animal-type "bird" :id b-id :type "button" :on-click (fn [_] (apply extract-attr _))}
+    "Click Me!"]]))
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1
       "Hello from " @name]
-     ]))
+     [button]
+     [sample-component]]))
